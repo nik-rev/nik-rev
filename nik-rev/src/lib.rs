0f7e60b97beb6dfcaf7249949de37466e77e89d1
Nik Revenco <pm@nikrev.com>
2026-02-24 12:09:26 +0000

    This script will be used in build.rs

--- nik-rev/src/lib.rs
+++ nik-rev/src/lib.rs
@@ -1,27 +1,25 @@
-#[macro_export]
-macro_rules! badges {
-    (repo = $repo_name:literal) => {
-        $crate::badges!(repo = $repo_name, msrv = env!("CARGO_PKG_RUST_VERSION"))
-    };
-    (repo = $repo_name:literal, msrv = $rust_version:expr) => {
-        concat!(
-            "[![crates.io](https://img.shields.io/crates/v/", env!("CARGO_PKG_NAME"), "?style=flat-square&logo=rust)](https://crates.io/crates/", env!("CARGO_PKG_NAME"), ")\n",
-            "[![docs.rs](https://img.shields.io/docsrs/", env!("CARGO_PKG_NAME"), "?style=flat-square&logo=docs.rs)](https://docs.rs/", env!("CARGO_PKG_NAME"), ")\n",
-            "![license](https://img.shields.io/badge/license-Apache--2.0_OR_MIT-blue?style=flat-square)\n",
-            "![msrv](https://img.shields.io/badge/msrv-", $rust_version, "-blue?style=flat-square&logo=rust)\n",
-            "[![github](https://img.shields.io/github/stars/nik-rev/", $repo_name, ")](https://github.com/nik-rev/", $repo_name, ")"
-        )
-    };
-}
+use std::env;
 
-#[macro_export]
-#[rustfmt::skip]
-macro_rules! add_dependency {
-    () => {
-        concat!(
-            "```toml\n",
-            env!("CARGO_CRATE_NAME"), " = \"",  env!("CARGO_PKG_VERSION_MAJOR"), ".", env!("CARGO_PKG_VERSION_MINOR"), "\"\n",
-            "```\n",
-        )
-    };
+pub fn export_env_vars_for_docs() {
+    let repo_name = env::var("CARGO_PKG_REPOSITORY").unwrap();
+    let repo_name = repo_name.strip_prefix("https://github.com/").unwrap();
+    let pkg_name = env::var("CARGO_PKG_NAME").unwrap();
+    let msrv = env::var("CARGO_PKG_RUST_VERSION").unwrap_or_else(|_| "nightly".to_string());
+
+    let badges = format!(
+            "[![crates.io](https://img.shields.io/crates/v/{pkg_name}?style=flat-square&logo=rust)](https://crates.io/crates/{pkg_name})
+[![docs.rs](https://img.shields.io/docsrs/{pkg_name}?style=flat-square&logo=docs.rs)](https://docs.rs/{pkg_name})
+![license](https://img.shields.io/badge/license-Apache--2.0_OR_MIT-blue?style=flat-square)
+![msrv](https://img.shields.io/badge/msrv-{msrv}-blue?style=flat-square&logo=rust)
+[![github](https://img.shields.io/github/stars/nik-rev/{repo_name})](https://github.com/nik-rev/{repo_name})"
+        );
+
+    let pkg_name = env::var("CARGO_PKG_NAME").unwrap();
+    let major = env::var("CARGO_PKG_VERSION_MAJOR").unwrap();
+    let minor = env::var("CARGO_PKG_VERSION_MINOR").unwrap();
+
+    let add_dep = format!("```toml\n{pkg_name} = \"{major}.{minor}\"\n```\n");
+
+    println!("cargo:rustc-env=GENERATED_BADGES={}", badges);
+    println!("cargo:rustc-env=GENERATED_ADD_DEP={}", add_dep);
 }
